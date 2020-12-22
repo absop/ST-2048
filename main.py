31432ac043b31369c68ebbc5c565291626a5db2f
absop <zlang0@163.com>
2020-12-22 16:01:25 +0800

    add ai

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
+import threading
 from random import randint
+from copy import deepcopy
 
 import sublime
 import sublime_plugin
@@ -14,51 +16,44 @@ import sublime_plugin
 sublime2048_record = "Sublime2048Record.sublime-settings"
 
 
-class Score():
-    def __init__(self):
-        self.current = 0
-        self.highest = 0
-
-    def increase(self, delta):
-        self.current += delta
-        self.highest = max(self.current, self.highest)
-
-class Grid():
-    def __init__(self, regions, record):
-        self.regions = regions
-        self.score = Score()
+class Board():
+    def __init__(self, record):
+        self.current_score = 0
+        self.highest_score = 0
         try:
             self.load(record)
         except:
             self.reset()
 
     def reset(self):
-        self.numbers = [[0 for j in range(4)] for i in range(4)]
+        self.matrix = [[0 for j in range(4)] for i in range(4)]
+        self.current_score = 0
         self.nblank = 16
         self.maxnum = 0
-        self.score.current = 0
         self.add_randnum()
 
     def load(self, record):
         try:
-            self.numbers = record["numbers"]
-            self.score.highest = record["score"]["highest"]
-            self.score.current = record["score"]["current"]
-            self.nblank = len([i for r in self.numbers for i in r if i == 0])
-            self.maxnum = max(max(self.numbers))
+            self.matrix = record["matrix"]
+            self.highest_score = record["highest_score"]
+            self.current_score = record["current_score"]
+            self.nblank = len([i for r in self.matrix for i in r if i == 0])
+            self.maxnum = max(max(self.matrix))
         except:
             self.reset()
-            self.score.highest = 0
+            self.highest_score = 0
 
     def record(self):
         return {
             "time": time.strftime("%Y%m%d %H:%M:%S"),
-            "numbers": self.numbers,
-            "score": {
-                "highest": self.score.highest,
-                "current": self.score.current
-            }
+            "matrix": self.matrix,
+            "highest_score": self.highest_score,
+            "current_score": self.current_score
         }
+
+    def accumulate_score(self, score):
+        self.current_score += score
+        self.highest_score = max(self.current_score, self.highest_score)
 
     def add_randnum(self):
         # 控制数字2和4的概率,此时2出现概率是80%
@@ -69,9 +64,9 @@ class Grid():
         count = 0
         for i in range(4):
             for j in range(4):
-                if self.numbers[i][j] == 0:
+                if self.matrix[i][j] == 0:
                     if count == randidx:
-                        self.numbers[i][j] = randnum
+                        self.matrix[i][j] = randnum
                         self.nblank -= 1
                         return (i, j)
                     count += 1
@@ -81,43 +76,43 @@ class Grid():
             return True
         for i in range(4):
             for j in range(1, 4):
-                if (self.numbers[i][j - 1] == self.numbers[i][j] or
-                    self.numbers[j - 1][i] == self.numbers[j][i]):
+                if (self.matrix[i][j - 1] == self.matrix[i][j] or
+                    self.matrix[j - 1][i] == self.matrix[j][i]):
                     return True
         return False
 
 
-def genmove(inc, begin, end, index):
-    def move(grid):
-        numbers = grid.numbers
+def genmove(array, swap):
+    def move(bodrd):
+        matrix = bodrd.matrix
         nblank, score, moved = 0, 0, 0
         for i in range(4):
-            k = begin
-            for j in range(begin, end, inc(0) - 0):
-                x1, y1 = index(i, k)
-                x2, y2 = index(i, j)
-                if j == k or numbers[x2][y2] == 0:
+            k = 0
+            for j in range(4):
+                x1, y1 = swap(i, array[k])
+                x2, y2 = swap(i, array[j])
+                if j == k or matrix[x2][y2] == 0:
                     continue
-                if numbers[x1][y1] == numbers[x2][y2]:
-                    numbers[x1][y1] <<= 1
-                    grid.maxnum = max(numbers[x1][y1], grid.maxnum)
-                    score += numbers[x1][y1]
+                if matrix[x1][y1] == matrix[x2][y2]:
+                    matrix[x1][y1] <<= 1
+                    bodrd.maxnum = max(matrix[x1][y1], bodrd.maxnum)
+                    score += matrix[x1][y1]
                     nblank += 1
-                    k = inc(k)
+                    k += 1
                 else:
-                    if numbers[x1][y1]:
-                        k = inc(k)
+                    if matrix[x1][y1]:
+                        k += 1
                         if k == j:
                             continue
-                    x1, y1 = index(i, k)
-                    numbers[x1][y1] = numbers[x2][y2]
+                    x1, y1 = swap(i, array[k])
+                    matrix[x1][y1] = matrix[x2][y2]
 
-                numbers[x2][y2] = 0
+                matrix[x2][y2] = 0
                 moved = 1
 
         if score > 0:
-            grid.nblank += nblank
-            grid.score.increase(score)
+            bodrd.nblank += nblank
+            bodrd.accumulate_score(score)
             return score
 
         return moved
@@ -125,30 +120,59 @@ def genmove(inc, begin, end, index):
     return move
 
 
-class Board():
+class Game():
     moves = {
-        "↑": genmove(lambda n: n + 1, 0, +4, lambda x, y: (y, x)),
-        "↓": genmove(lambda n: n - 1, 3, -1, lambda x, y: (y, x)),
-        "←": genmove(lambda n: n + 1, 0, +4, lambda x, y: (x, y)),
-        "→": genmove(lambda n: n - 1, 3, -1, lambda x, y: (x, y))
+        "↑": genmove([0, 1, 2, 3], lambda x, y: (y, x)),
+        "↓": genmove([3, 2, 1, 0], lambda x, y: (y, x)),
+        "←": genmove([0, 1, 2, 3], lambda x, y: (x, y)),
+        "→": genmove([3, 2, 1, 0], lambda x, y: (x, y))
     }
-    def __init__(self, region, record={}):
-        self.grid = Grid(region, record)
+    def __init__(self, record={}):
+        self.bodrd = Board(record)
 
     def best(self):
-        return self.grid.score.highest
+        return self.bodrd.highest_score
 
     def score(self):
-        return self.grid.score.current
+        return self.bodrd.current_score
 
     def move(self, direction):
-        return self.moves[direction](self.grid)
+        return self.moves[direction](self.bodrd)
 
     def isalive(self):
-        return self.grid.movable()
+        return self.bodrd.movable()
 
     def iswon(self):
-        return self.grid.maxnum >= 2048
+        return self.bodrd.maxnum >= 2048
+
+    def get_best_key(self, total_simulation_times=200):
+        simulation_times = int(total_simulation_times / 4)
+        possible_directions = ["↑", "↓", "←", "→"]
+        cumulate_scores = {}
+        simulator = Game()
+        for direction in possible_directions:
+            cumulate_scores[direction] = 0
+            for i in range(simulation_times):
+                simulator.bodrd = deepcopy(self.bodrd)
+                moved = simulator.move(direction)
+                if not moved:
+                    break
+                cumulate_scores[direction] += moved
+                simulator.bodrd.add_randnum()
+                while simulator.isalive():
+                    d = possible_directions[randint(0, 3)]
+                    moved = simulator.move(d)
+                    if moved:
+                        cumulate_scores[direction] += moved
+                        simulator.bodrd.add_randnum()
+
+        best_d, best_s = possible_directions[0], 0
+        for direction in possible_directions:
+            if cumulate_scores[direction] > best_s:
+                best_s = cumulate_scores[direction]
+                best_d = direction
+
+        return best_d
 
 
 class Sublime2048(sublime_plugin.TextCommand):
@@ -187,6 +211,22 @@ class Sublime2048(sublime_plugin.TextCommand):
         if command == "move":
             self.move(edit, key)
 
+        elif command == "auto_move":
+            if not self.game.isalive():
+                self.stop_ai()
+                return
+            def threaded_auto_run():
+                key = self.game.get_best_key()
+                self.view.run_command(
+                    'sublime2048', {'command': 'move', 'key': key})
+                if self.view.settings().get('play2048_ai', False):
+                    sublime.set_timeout(
+                        lambda: self.view.run_command(
+                            'sublime2048', {'command': 'auto_move'}),
+                        0)
+            thread = threading.Thread(target=threaded_auto_run)
+            thread.start()
+
         elif command == "reset":
             self.game_resart(edit)
 
@@ -198,13 +238,43 @@ class Sublime2048(sublime_plugin.TextCommand):
             self.create_game_board(self.view)
             record = self.load_record()
 
-        self.regions = self.board_regions()
-        self.board = Board(self.regions["cell"], record)
+        score_region = self.view.find(" SCORE ", 0)
+        head_line_region = self.view.line(score_region)
+        line_length = head_line_region.size() + 1
+        pt = head_line_region.end() + 2 * line_length
+        self.annotation_region = sublime.Region(pt, pt)
+
+        start_point = score_region.begin() + 2 * line_length
+        self.caption_regions = {
+            self.game_captions[i]: sublime.Region(
+                start_point + i * 8, start_point + i * 8 + 7)
+            for i in range(4)
+        }
+
+        self.tile_regions = []
+        for i in range(4):
+            self.tile_regions.append([])
+            tile_start_point = score_region.begin() + (5 + i * 4) * line_length
+            for j in range(4):
+                self.tile_regions[i].append(
+                    tuple(
+                        sublime.Region(a, a + 7)
+                        for a in range(tile_start_point,
+                            tile_start_point + 3 * line_length, line_length)
+                    )
+                )
+                tile_start_point += 8
+
+        self.game = Game(record)
         self.moved = 0
         self.arrow = None
-        self.game_overed = not self.board.isalive()
+        self.game_overed = not self.game.isalive()
+
+        if not self.view.settings().get('play2048_ai', False):
+            self.stop_ai()
         if self.game_overed:
             self.game_over(edit)
+
         self.refresh(edit)
 
     def create_game_board(self, view):
@@ -217,46 +287,47 @@ class Sublime2048(sublime_plugin.TextCommand):
         view.settings().set("draw_white_space", None)
         view.run_command('append', {'characters': self.game_board_text})
 
-    def board_regions(self):
-        region = self.view.find(" SCORE ", 0)
-        length = self.view.line(region).size() + 1
+    def run_ai(self):
+        self.view.settings().set('play2048_ai', True)
+        # self.add_ai_annotation('stop_ai', '停止')
+        self.add_ai_annotation('stop_ai', '✘')
+        self.view.run_command('sublime2048', {'command': 'auto_move'})
 
-        start_point = region.begin() + 2 * length
-        regions = {
-            self.game_captions[i]: sublime.Region(
-                start_point + i * 8, start_point + i * 8 + 7)
-            for i in range(4)
-        }
+    def stop_ai(self):
+        self.view.settings().set('play2048_ai', False)
+        # self.add_ai_annotation('run_ai', '自动')
+        self.add_ai_annotation('run_ai', '➜')
 
-        grid_regions = regions["grid"] = []
-        cell_regions = regions["cell"] = []
-        for i in range(4):
-            grid_regions.append([])
-            cell_regions.append([])
-            grid_start_point = region.begin() + (5 + i * 4) * length
-            for j in range(4):
-                grid_regions[i].append(tuple(
-                    sublime.Region(a, a + 7)
-                    for a in range(grid_start_point,
-                        grid_start_point + 3 * length, length)
-                ))
-                cell_regions[i].append(grid_regions[i][j][1])
-                grid_start_point += 8
+    def add_ai_annotation(self, href, img):
+        content = """
+            <span class="label label-success"><a href="%s">%s</a></span>
+        """ % (href, img)
+        self.view.erase_phantoms('Play2048')
+        self.view.add_phantom(
+            'Play2048',
+            self.annotation_region,
+            self.html.format(css=self.css, content=content),
+            sublime.LAYOUT_INLINE, self.on_navigate
+        )
 
-        return regions
+    def on_navigate(self, href):
+        if href == 'run_ai':
+            self.run_ai()
+        else:
+            self.stop_ai()
 
     def move(self, edit, key):
         self.arrow = key
         if not self.game_overed:
-            self.moved = self.board.move(key)
+            self.moved = self.game.move(key)
             if self.moved:
-                row, col = self.board.grid.add_randnum()
-                if not self.board.isalive():
+                row, col = self.game.bodrd.add_randnum()
+                if not self.game.isalive():
                     self.game_over(edit)
-                if self.board.iswon():
+                if self.game.iswon():
                     self.game_won(edit)
 
-                record = self.board.grid.record()
+                record = self.game.bodrd.record()
                 self.view.settings().set("record", record)
 
                 self.refresh(edit, row=row, col=col)
@@ -267,7 +338,7 @@ class Sublime2048(sublime_plugin.TextCommand):
     def game_resart(self, edit):
         self.status_message(edit, "")
         self.game_overed = False
-        self.board.grid.reset()
+        self.game.bodrd.reset()
         self.refresh(edit)
 
     def game_over(self, edit):
@@ -294,33 +365,32 @@ class Sublime2048(sublime_plugin.TextCommand):
 
         self.view.set_read_only(False)
 
-        self.view.replace(edit, self.regions["score"],
-                          align("%d" % self.board.score()))
-        self.view.replace(edit, self.regions["best"],
-                          align("%d" % self.board.best()))
-        self.view.replace(edit, self.regions["key"],
+        self.view.replace(edit, self.caption_regions["score"],
+                          align("%d" % self.game.score()))
+        self.view.replace(edit, self.caption_regions["best"],
+                          align("%d" % self.game.best()))
+        self.view.replace(edit, self.caption_regions["key"],
                           align(self.arrow or ""))
-        self.view.replace(edit, self.regions["got"],
+        self.view.replace(edit, self.caption_regions["got"],
                           formater("%+d", self.moved))
 
         for x in range(4):
             for y in range(4):
-                number = self.board.grid.numbers[x][y]
-                region = self.board.grid.regions[x][y]
+                number = self.game.bodrd.matrix[x][y]
+                region = self.tile_regions[x][y][1]
                 key = "%d_%d_sublime2048" % (x, y)
                 self.view.replace(edit, region, formater("%d", number))
                 self.view.erase_regions(key)
-                self.view.add_regions(key,
-                    self.regions["grid"][x][y],
+                self.view.add_regions(key, self.tile_regions[x][y],
                     scope="%s.sublime2048" % (number or "empty"),
                     flags=sublime.DRAW_NO_OUTLINE|sublime.PERSISTENT)
 
         self.view.erase_regions("new_sublime2048")
         if row is not None:
             self.view.erase_regions("%d_%d_sublime2048" % (row, col))
-            number = self.board.grid.numbers[row][col]
+            number = self.game.bodrd.matrix[row][col]
             self.view.add_regions("new_sublime2048",
-                self.regions["grid"][row][col],
+                self.tile_regions[row][col],
                 scope="%d.sublime2048 new_number.sublime2048" % number,
                 flags=sublime.DRAW_NO_OUTLINE|sublime.PERSISTENT)
 
@@ -346,6 +416,8 @@ class Sublime2048Manager(sublime_plugin.ViewEventListener):
         return settings.has("sublime2048")
 
     def on_query_context(self, key, operator, operand, match_all):
+        if self.view.settings().get('play2048_ai', False):
+            return False
         return key == "play2048"
 
     def on_selection_modified(self):
@@ -368,3 +440,9 @@ class Sublime2048Manager(sublime_plugin.ViewEventListener):
         settings = sublime.load_settings(sublime2048_record)
         settings.set("record", record)
         sublime.save_settings(sublime2048_record)
+
+
+def plugin_loaded():
+    load_resource = sublime.load_resource
+    Sublime2048.css = load_resource("Packages/Sublime2048/html/ui.css")
+    Sublime2048.html = load_resource("Packages/Sublime2048/html/ui.html")
